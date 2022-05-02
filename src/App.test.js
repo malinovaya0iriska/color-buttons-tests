2a78f8507804476cfdc1d8db3c28e5c2da4334cf
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-02 10:09:06 +0300

    add new test case to check accessibility

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn react');
+  const linkElement = screen.getByText('Learn React');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('check if learn react link is link', () => {
+  render(<App />);
+  const linkElement = screen.getByRole('link', {name: /learn react/i});
   expect(linkElement).toBeInTheDocument();
 });
