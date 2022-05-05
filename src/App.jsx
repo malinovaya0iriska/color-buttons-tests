530ae0fcfe529ab5f7904dc778fefb56bb616b2d
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-05 17:54:54 +0300

    add eslint and prettier

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import { useState } from 'react';
+
+export const replaceCamelCaseWithSpaces = (colorName) =>
+  colorName.replace(/\B([A-Z])\B/g, ' $1');
+
+function App() {
+  const [buttonColor, setButtonColor] = useState('MediumVioletRed');
+  const [disabled, setDisabled] = useState(false);
+
+  const newButtonColor =
+    buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+  return (
+    <div>
+      <button
+        style={{
+          backgroundColor: disabled ? 'gray' : buttonColor,
+          color: '#fff',
+        }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {replaceCamelCaseWithSpaces(newButtonColor)}
+      </button>
+      <input
+        type='checkbox'
+        id='disable-button'
+        defaulValue={disabled}
+        area-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor='disable-button'>Disable button</label>
+    </div>
+  );
+}
+
+export default App;
